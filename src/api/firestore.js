80f41d52dd0d520e0955249aee889d90bb069a28
Seokho <seokho0120@gmail.com>
2023-10-31 20:27:43 +0900

    fix: 테스트 코드 삭제

--- src/api/firestore.js
+++ src/api/firestore.js
@@ -1,6 +1,15 @@
-import firebasedb from './firebasedb';
+import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-const fireStore = getFirestore(firebasedb);
+const firebaseConfig = {
+  apiKey: 'AIzaSyDLwwhSzGE_U5Ee7exVp5ak9PdYFD-HTeg',
+  authDomain: 'peachbloom-27ec8.firebaseapp.com',
+  projectId: 'peachbloom-27ec8',
+  storageBucket: 'peachbloom-27ec8.appspot.com',
+  messagingSenderId: '399974555241',
+  appId: '1:399974555241:web:d94e5808f90f071f2cccf8',
+  measurementId: 'G-SFRSWH0MW7',
+};
 
-export default fireStore;
+const firebasedb = initializeApp(firebaseConfig);
+const fireStore = getFirestore(firebasedb);
