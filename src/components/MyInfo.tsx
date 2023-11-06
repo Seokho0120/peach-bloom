0ea62873985479fdd391c4fe48ed35834bb85daa
Seokho <seokho0120@gmail.com>
2023-11-07 02:10:43 +0900

    add: mypage 스타일링

--- src/components/MyInfo.tsx
+++ src/components/MyInfo.tsx
@@ -11,31 +11,43 @@ export default function MyInfo() {
   };
 
   return (
-    <div className='flex flex-col gap-10'>
-      <div className='flex gap-2 items-baseline'>
+    <div className='flex flex-col gap-10 w-full'>
+      <div className='flex gap-3 items-baseline'>
         <h2 className='text-4xl font-bold'>{user?.name} 님</h2>
         <button
           onClick={handleSignOut}
-          className='text-sm text-navypoint hover:text-pinkpoint'
+          className='text-navypoint hover:text-pinkpoint'
         >
           로그아웃
         </button>
       </div>
 
-      <div className='flex flex-col gap-4'>
-        <h3>기본정보</h3>
-        <div className='flex'>
-          {/*eslint-disable-next-line @next/next/no-img-element*/}
-          <img alt='user profile' src={user?.image ?? undefined} />
-          <p>{user?.name}</p>
-        </div>
-        <div className='flex'>
-          <p>닉네임</p>
-          <p>{user?.username}</p>
-        </div>
-        <div className='flex'>
-          <p>이메일</p>
-          <p>{user?.email}</p>
+      <div className='flex flex-col gap-4 bg-gray-50 p-10'>
+        <h3 className='text-2xl font-bold'>기본정보</h3>
+        <div className='bg-white p-8 flex flex-col'>
+          <div className='flex items-center gap-10 mb-6'>
+            {/*eslint-disable-next-line @next/next/no-img-element*/}
+            <img
+              className='rounded-full w-14'
+              alt='user profile'
+              src={user?.image ?? undefined}
+            />
+            <p className='text-xl font-semibold'>{user?.name}</p>
+          </div>
+
+          <div className='flex items-center gap-8 mb-6'>
+            <span className='text-lg text-slate-400 basis-1/12 ml-1'>
+              닉네임
+            </span>
+            <p className='border-b basis-11/12 leading-7'>{user?.username}</p>
+          </div>
+
+          <div className='flex items-center gap-8 mb-6'>
+            <span className='text-lg text-slate-400 basis-1/12 ml-1'>
+              이메일
+            </span>
+            <p className='border-b basis-11/12 leading-7'>{user?.email}</p>
+          </div>
         </div>
       </div>
     </div>
