9717e6dc097a99ccdaa653aa971eaddc16673797
Seokho <seokho0120@gmail.com>
2023-11-20 20:15:09 +0900

    fix: RecoilEnv삭제

--- src/atoms/productsListAtom.ts
+++ src/atoms/productsListAtom.ts
@@ -1,6 +1,5 @@
-import { atom, RecoilEnv } from 'recoil';
+import { atom } from 'recoil';
 import { ProductListType, ProductDetailType } from '../types/Product';
-RecoilEnv.RECOIL_DUPLICATE_ATOM_KEY_CHECKING_ENABLED = false;
 
 // 초기값
 export const initialProductsListAtom = atom<ProductListType[]>({
