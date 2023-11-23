ea96b3915545ff742e7167a0890475441bc00464
Seokho <seokho0120@gmail.com>
2023-11-23 18:10:23 +0900

    add: sharp 패키지 설치, remotePatterns 설정

--- next.config.js
+++ next.config.js
@@ -6,7 +6,20 @@ const nextConfig = {
   images: {
     imageSizes: [100, 200, 300, 400, 500],
     deviceSizes: [580, 1024, 1284, 1400, 1600, 1800],
-    domains: ['res.cloudinary.com', 'dn5hzapyfrpio.cloudfront.net'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'dn5hzapyfrpio.cloudfront.net',
+        port: '',
+        pathname: '/**',
+      },
+    ],
   },
 };
 
