5f1f75cdb62fea58e8fe927609000f437ddffed9
Seokho <seokho0120@gmail.com>
2024-01-07 18:28:38 +0900

    fix: recoil 원복

--- src/atoms/ProductsAtom.ts
+++ src/atoms/ProductsAtom.ts
@@ -1,14 +1,5 @@
 import { atom } from 'recoil';
 import { ProductListType } from '../types/ProductType';
-import { recoilPersist } from 'recoil-persist';
-
-const sessionStorage =
-  typeof window !== 'undefined' ? window.sessionStorage : undefined;
-
-const { persistAtom } = recoilPersist({
-  key: 'test',
-  storage: sessionStorage,
-});
 
 // 초기값
 export const initialProductsListAtom = atom<ProductListType[]>({
@@ -19,7 +10,6 @@ export const initialProductsListAtom = atom<ProductListType[]>({
 export const productsListAtom = atom<ProductListType[]>({
   key: 'productsListAtom',
   default: [],
-  effects_UNSTABLE: [persistAtom],
 });
 
 export const discountedPriceAtom = atom<number>({
